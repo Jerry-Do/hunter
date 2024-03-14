a7539a1192334f421cd78dec4128cb6df74cecde
Thanhle26397 <69609831+Thanhle26397@users.noreply.github.com>
2024-03-13 21:10:57 -0400

    Update register.cs

--- Assets/scripts/login/register.cs
+++ Assets/scripts/login/register.cs
@@ -3,6 +3,9 @@ using MongoDB.Driver;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Security.Cryptography;
+using System.Text;
+using System;
 
 public class register : MonoBehaviour
 {
@@ -11,8 +14,8 @@ public class register : MonoBehaviour
     private IMongoCollection<BsonDocument> collection;
 
 
-    private string email;
-    private string password;
+    public string email;
+    public string password;
     int n, m;
     // Start is called before the first frame update
     void Start()
@@ -20,7 +23,7 @@ public class register : MonoBehaviour
         // Initialize MongoDB connection
         client = new MongoClient("mongodb+srv://esomeh:ZndxWXyeRBTpe2GG@senecaweb.7jxhv5v.mongodb.net/?retryWrites=true&w=majority");
         database = client.GetDatabase("RegisterDB");
-        collection = database.GetCollection<BsonDocument>("users");
+        collection = database.GetCollection<BsonDocument>("RegisterUnityCollection");
     }
 
     // Update is called once per frame
@@ -37,7 +40,7 @@ public class register : MonoBehaviour
 
     public void readPasswordInput(string passwordInput)
     {
-        password = passwordInput;
+        password = HashPassword(passwordInput);
         Debug.Log(password);
     }
     // navigate login screen
@@ -48,15 +51,35 @@ public class register : MonoBehaviour
         //SceneManager.LoadScene("login");
     }
     // register function
-    public void OnRegisterButtonPress()
+
+    public string HashPassword(string password)
     {
-        var newUser = new BsonDocument
+        using (SHA256 sha256 = SHA256.Create())
         {
-            {"email", email},
-            {"password", password} // Consider hashing the password before storing
-        };
-        m++;
-        Debug.Log("Register Button clicked " + m + " times.");
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
     }
-    
+    public async void OnRegisterButtonPress()
+    {
+        var newUser = new BsonDocument{
+        {"email", email},
+        {"password", password}
+        };
+
+        try
+        {
+            await collection.InsertOneAsync(newUser);
+            Debug.Log("User registered successfully.");
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"An error occurred: {ex.Message}");
+        }
+    }
 }
